9673655b441d6df5be66ce44c27ac102e6c8f08a
Lili <pastrnakie@seznam.cz>
2021-03-25 18:18:56 +0100

    fix level requirements, level picker

--- adaptive_game_module/level_selector.py
+++ adaptive_game_module/level_selector.py
@@ -1,9 +1,9 @@
 import yaml  # reads hint configurations from a .yml file
 
 class LevelSelector:
-    def __init__(self, tool_list_filename):
-        self.read_level_requirements(filename)
-        self.read_tool_list(filename)
+    def __init__(self, tool_list_filename, level_requirements_filename):
+        self.read_level_requirements(level_requirements_filename)
+        self.read_tool_list(tool_list_filename)
         self.known_tools = []
 
     def read_level_requirements(self, filename):  
@@ -20,14 +20,14 @@ class LevelSelector:
         self.known_tools.append(tool)
 
     def next_level(self, current_level, current_time):
-        next_levels = self.level_requirements[level]
+        next_levels = self.level_requirements["level" + str(current_level+1)]
         if len(next_levels) == 1:
             return next_levels[0][0]
         candidate_level = None
-        for {level, requirements} in next_levels:
+        for level, requirements in next_levels:
             if requirements is None:
                 candidate_level = level
-            elif ((requirements[0] < current_time) and (requirements[1] in self.known_tools)):
+            elif ((requirements[0] > current_time) and (requirements[1] in self.known_tools)):
                 candidate_level = level
             else:
                 pass
